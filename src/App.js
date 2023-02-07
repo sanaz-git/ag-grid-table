7d0e1e2f8f3e346f8c7a7213d61ebfdfb1934876
Sanaz <szeraati69@gmail.com>
2023-02-07 23:13:48 +0330

    create simple table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import { useState ,useEffect} from 'react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function App() {
+
+  const [rowData,setRowData] = useState([
+    {make: 'ford', model:'Focus', price:40000},
+    {make: 'Toyota', model:'Celica', price:40000},
+    {make: 'BMW', model:'4 Series', price:40000}
+  ]);
+
+  const [columnDefs,setColumnDefs]=useState([
+    {field:'make'},
+      {field:'model'},
+      {field:'price'}
+  ]);
+
+
+
   return (
-    <div className="App">
-      App
+    <div className="ag-theme-alpine" style={{height:500}}>
+      <AgGridReact
+      rowData={rowData}
+      columnDefs={columnDefs}
+      />
     </div>
   );
 }
