3f04b16b0ecd2b92708b9283242a93fb33d36a0a
Sanaz <szeraati69@gmail.com>
2023-02-08 18:53:44 +0330

    complete table with usestste useeffect usememo usecallback use ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import { AgGridReact } from 'ag-grid-react';
-import { useState ,useEffect} from 'react';
+import { useState ,useEffect, useMemo, useCallback, useRef} from 'react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
+
 function App() {
+
+  const gridRef =useRef();
 
   const [rowData,setRowData] = useState([
     {make: 'ford', model:'Focus', price:40000},
@@ -19,13 +22,35 @@ function App() {
       {field:'price'}
   ]);
 
+  const defaultColDef = useMemo(()=> ({
+    sortable:true,
+    filter:true
+  }))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData))
+  }, []);
 
+  const cellClickedListener = useCallback(e => {
+  console.log('cellClicked', e)
+  })
+
+  const pushMeClicked = useCallback (e => {
+    gridRef.current.api.deselectAll();
+  });
 
   return (
     <div className="ag-theme-alpine" style={{height:500}}>
+      <button onClick={pushMeClicked}>Push Me</button>
       <AgGridReact
+      ref={gridRef}
+      onCellClicked={cellClickedListener}
       rowData={rowData}
       columnDefs={columnDefs}
+      defaultColDef={defaultColDef}
+      rowSelection='multiple'
+      animateRows={true}
       />
     </div>
   );
